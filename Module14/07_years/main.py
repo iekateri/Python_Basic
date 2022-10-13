605595931eac06f1aadeb2de3a12a117a93c971f
MakarychevaE <ekaterina.makarycheva@daimler.com>
2022-10-13 10:41:04 +0300

    задача решена

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+if __name__ == '__main__':
+    a = int(input('Введите первый год: '))
+    b = int(input('Введите второй год: '))
+    while a < b:
+        for i in range(10):
+            if 3 == str(a).count(str(i)):
+                print(a)
+        a += 1
