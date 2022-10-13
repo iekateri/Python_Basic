2f9a08c30df8c6cf3c4bd2c6e05c29a291469e16
MakarychevaE <ekaterina.makarycheva@daimler.com>
2022-10-13 10:05:10 +0300

    функции написаны

--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+def sum_nums(n):
+    list_num = list(str(n))
+    summ = 0
+    for num in list_num:
+        summ += int(num)
+    return summ
+
+
+def count_nums(n):
+    list_num = list(str(n))
+    return len(list_num)
+
+
+if __name__ == '__main__':
+    N = int(input('Введите число: '))
+    a = sum_nums(N)
+    b = count_nums(N)
+    print(f'Сумма цифр: {a}')
+    print(f'Количество цифр в числе: {b}')
+    print(f'Разность суммы и количества цифр: {a - b}')
