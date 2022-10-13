4c9b8d1f1f441ca145efc3118e9367350445e107
MakarychevaE <ekaterina.makarycheva@daimler.com>
2022-10-13 10:20:47 +0300

    функция готова

--- Module14/04_reverse_num/main.py
+++ Module14/04_reverse_num/main.py
@@ -1,3 +1,12 @@
-# TODO здесь писать код
+def reverse_float(num):
+    parts = str(num).split('.')
+    reversed_parts = [''.join(reversed(part)) for part in parts]
+    return float('.'.join(reversed_parts))
 
 
+if __name__ == '__main__':
+    N = float(input('Введите первое число: '))
+    K = float(input('Введите второе число: '))
+    print(f'Первое число наоборот: {reverse_float(N)}')
+    print(f'Второе число наоборот: {reverse_float(K)}')
+    print(f'Сумма: {reverse_float(N)+reverse_float(K)}')
