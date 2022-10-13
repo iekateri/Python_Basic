483575ef1b6763d0da2359bf49032f38b2f2e328
MakarychevaE <ekaterina.makarycheva@daimler.com>
2022-10-13 10:32:13 +0300

    функция готова

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+if __name__ == '__main__':
+    num = int(input('Введите число: '))
+    i = 1
+    while i <= num:
+        i = i + 1
+        if num % i == 0:
+            print('Наименьший делитель, отличный от единицы:', i)
+            break
